1ae362a3edc9f1a2ac0bee9c775632b24e9bf3a5
Saif khan <57302600+saifkhan199@users.noreply.github.com>
2019-12-21 22:01:41 +0500

    Merging Error Resolved

--- Mart Management System/Mart Management System/Program.cs
+++ Mart Management System/Mart Management System/Program.cs
@@ -16,11 +16,8 @@ namespace Mart_Management_System
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-<<<<<<< HEAD
-            Application.Run(new Cashier());
-=======
             Application.Run(new LoginForm());
->>>>>>> 108eb6d31aa072fc1945322443f643f27ee6347c
+
         }
     }
 }
